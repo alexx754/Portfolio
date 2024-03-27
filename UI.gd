ce0a4bb6f603685dbc9e4f46553767417d7939cd
alexx <119589342+alexx754@users.noreply.github.com>
2024-03-27 23:52:51 +0100

    Delete UI.gd

--- UI.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-
-
-
-@onready var label = $Label
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	label.text = "Wood: " + str(Game.Wood)
